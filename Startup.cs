51aafcb88e5e664b7c53ac316863863b1952de4e
pl1965 <vsnu97@gmail.com>
2022-08-24 12:10:07 +0530

    Added corse.

--- Startup.cs
+++ Startup.cs
@@ -62,6 +62,10 @@ namespace AsterMimsWebApplication2022
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options =>
+            options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
+            );
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
